4e5f01f472a79a2a74c2c91c449798778a368968
Samet Candan <c._samet@hotmail.com>
2024-03-02 23:32:40 +0300

    toppings test

--- src/components/Toppings/Toppings.test.js
+++ src/components/Toppings/Toppings.test.js
@@ -1,6 +1,32 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Toppings from ".";
+import userEvent from "@testing-library/user-event";
 
-test("ccc", () => {
+test("sosların ekleme çıkarma işleminin toplam fiyata olan etkisi", async () => {
+  const user = userEvent.setup();
   render(<Toppings />);
+
+  const total = screen.getByRole("heading", { name: /Sos Ücreti:/i });
+
+  const cherryCheck = await screen.findByRole("checkbox", {
+    name: /Cherries/i,
+  });
+
+  await user.click(cherryCheck);
+
+  expect(total).toHaveTextContent("3");
+
+  const mochiCheck = await screen.findByRole("checkbox", { name: /Mochi/i });
+
+  await user.click(mochiCheck);
+
+  expect(total).toHaveTextContent("6");
+
+  await user.click(cherryCheck);
+
+  expect(total).toHaveTextContent("3");
+
+  await user.click(mochiCheck);
+
+  expect(total).toHaveTextContent("0");
 });
--- src/components/Toppings/index.jsx
+++ src/components/Toppings/index.jsx
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 
 const Toppings = () => {
   const [toppingData, setToppingData] = useState([]);
+  const [basket, setBasket] = useState([]);
+
+  const handleChange = (e, topping) => {
+    e.target.checked
+      ? setBasket([...basket, topping])
+      : setBasket(basket.filter((item) => item.name !== topping.name));
+  };
   useEffect(() => {
     axios
       .get("http://localhost:3050/toppings")
@@ -11,15 +18,22 @@ const Toppings = () => {
   return (
     <div className="container my-5">
       <h1>Sos Çeşitleri</h1>
-      <p>Tanesi 2&#8378;</p>
-      <h2>Sos Ücreti:</h2>
-      <div>
+      <p>Tanesi 3&#8378;</p>
+      <h2>Sos Ücreti:{basket.length * 3}</h2>
+      <div className="row gap-3 mt-4">
         {toppingData.map((topping, i) => (
-          <div key={i}>
-            <img src={topping.imagePath} alt="" />
-            <label className="text-nowrap">{topping.name}</label>
+          <div
+            style={{ width: "150px" }}
+            className="d-flex flex-column align-items-center"
+            key={i}
+          >
+            <img className="img-fluid" src={topping.imagePath} alt="" />
+            <label htmlFor={topping.name} className="text-nowrap">
+              {topping.name}
+            </label>
             <input
               className="form-check-input"
+              onChange={(e) => handleChange(e, topping)}
               type="checkbox"
               name=""
               id={topping.name}
